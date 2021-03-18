184f867f79297f728aeb7a87c33ca75ab3add055
elasota <ejlasota@gmail.com>
2021-03-18 17:03:52 -0400

    Fix Android symlinks

--- AerofoilAndroid/remove_symlinks.bat
+++ AerofoilAndroid/remove_symlinks.bat
@@ -11,7 +11,6 @@ rmdir app\jni\GpApp
 rmdir app\jni\GpFontHandler_FreeType2
 rmdir app\jni\PortabilityLayer
 rmdir app\jni\FreeType
-rmdir app\jni\zlib
 rmdir app\jni\rapidjson
 rmdir app\jni\MacRomanConversion
 rmdir app\jni\stb
