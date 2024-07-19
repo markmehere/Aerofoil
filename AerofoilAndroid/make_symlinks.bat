9613299717eb791f293c2a1a0802fb34d7480aaa
elasota <1137273+elasota@users.noreply.github.com>
2024-07-19 19:10:06 -0400

    Update to SDL 2.30.5

--- AerofoilAndroid/make_symlinks.bat
+++ AerofoilAndroid/make_symlinks.bat
@@ -7,7 +7,7 @@ call remove_symlinks.bat
 mklink /D app\jni\AerofoilSDL ..\..\..\AerofoilSDL
 mklink /D app\jni\AerofoilPortable ..\..\..\AerofoilPortable
 mklink /D app\jni\Common ..\..\..\Common
-mklink /D app\jni\SDL2 ..\..\..\SDL2-2.0.12
+mklink /D app\jni\SDL2 ..\..\..\SDL2-2.30.5
 mklink /D app\jni\GpApp ..\..\..\GpApp
 mklink /D app\jni\GpShell ..\..\..\GpShell
 mklink /D app\jni\GpCommon ..\..\..\GpCommon
