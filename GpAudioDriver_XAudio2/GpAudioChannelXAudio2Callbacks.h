c933e11f5e720195215300ef7962510da94c0d76
elasota <1137273+elasota@users.noreply.github.com>
2024-08-03 23:39:16 -0400

    Fix calling convention in Win32

--- GpAudioDriver_XAudio2/GpAudioChannelXAudio2Callbacks.h
+++ GpAudioDriver_XAudio2/GpAudioChannelXAudio2Callbacks.h
@@ -9,13 +9,13 @@ class GpAudioChannelXAudio2Callbacks final : public IXAudio2VoiceCallback
 public:
 	explicit GpAudioChannelXAudio2Callbacks(GpAudioChannelXAudio2 *owner);
 
-	void OnVoiceProcessingPassStart(UINT32 BytesRequired) override;
-	void OnVoiceProcessingPassEnd() override;
-	void OnStreamEnd() override;
-	void OnBufferStart(void* pBufferContext) override;
-	void OnBufferEnd(void* pBufferContext) override;
-	void OnLoopEnd(void* pBufferContext) override;
-	void OnVoiceError(void* pBufferContext, HRESULT Error) override;
+	void STDMETHODCALLTYPE OnVoiceProcessingPassStart(UINT32 BytesRequired) override;
+	void STDMETHODCALLTYPE OnVoiceProcessingPassEnd() override;
+	void STDMETHODCALLTYPE OnStreamEnd() override;
+	void STDMETHODCALLTYPE OnBufferStart(void* pBufferContext) override;
+	void STDMETHODCALLTYPE OnBufferEnd(void* pBufferContext) override;
+	void STDMETHODCALLTYPE OnLoopEnd(void* pBufferContext) override;
+	void STDMETHODCALLTYPE OnVoiceError(void* pBufferContext, HRESULT Error) override;
 
 private:
 	GpAudioChannelXAudio2 *m_owner;
