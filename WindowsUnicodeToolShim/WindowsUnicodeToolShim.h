e2fcd7851df5ea7488a8b3536cda7a84e49f7b64
Diomendius <42310725+Diomendius@users.noreply.github.com>
2024-08-05 15:46:24 +1200

    Add UnixUnicodeToolShim.cpp
    This is the counterpart to WindowsUnicodeToolShim.cpp for *nix systems.

--- WindowsUnicodeToolShim/WindowsUnicodeToolShim.h
+++ WindowsUnicodeToolShim/WindowsUnicodeToolShim.h
@@ -8,6 +8,7 @@ int mkdir_utf8(const char *path);
 void TerminateDirectoryPath(std::string &path);
 void ScanDirectoryForExtension(std::vector<std::string>& outPaths, const char *path, const char *ending, bool recursive);
 
+#ifdef _WIN32
 struct DirectoryScanContext;
 struct DirectoryScanEntry
 {
@@ -17,3 +18,11 @@ struct DirectoryScanEntry
 DirectoryScanContext *opendir_utf8(const char *name);
 DirectoryScanEntry *readdir_utf8(DirectoryScanContext *dir);
 void closedir_utf8(DirectoryScanContext *context);
+
+#else
+
+inline int _fseeki64(FILE *stream, long offset, int whence) {
+	return fseek(stream, offset, whence);
+}
+inline long _ftelli64(FILE *stream) { return ftell(stream); };
+#endif
